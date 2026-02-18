20dbb7f21d5352ffe569635df0d0260529e556ec
Nishkarsh agarwal <aggarwalnishkarsh@gmail.com>
2026-02-18 17:00:57 +0530

    second code push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Navbar.css';
 import Logo from '../images/logo.png';
+import {FaBars, FaTimes} from 'react-icons/fa';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
   return (
     <div className='navbar'>
         <div className='logo'>
             <img src={Logo} alt='logo' />
         </div>
-        <ul></ul>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'><a>Home</a></li>
+            <li className='nav-item'><a>About</a></li>
+            <li className='nav-item'><a>FAQ</a></li>
+            <li className='nav-item'><a>Contact</a></li>
+        </ul>
+        <div className='hamburger' onClick={handleClick}>
+            {click ? (<FaTimes  size={30} style={{color: '#fff'}} />) : (<FaBars size={30} style={{color: '#fff'}} />)}
+            
+        </div>
     </div>
   )
 }
